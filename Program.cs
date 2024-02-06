b87359335c7d86d2c46257425c955650ef6938c2
zeropirata <gabriel0gsbn@gmail.com>
2024-02-06 19:14:36 -0300

    Implementações faltando do desafio de POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia building...");
+Smartphone nokia = new Nokia(numero: "4002-8922", modelo: "C24", imei: "123", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+Console.WriteLine("\n");
+Console.WriteLine("Iphone building...");
+Smartphone iphone = new Iphone(numero: "4002-8922", modelo: "42", imei: "123", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("FaceBook");
